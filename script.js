e9b6d6d99f3efdb16b8c68b01d56163048cdca4c
Sabina Andersson <sabina.andersson@medieinstitutet.se>
2021-12-03 16:23:53 +0100

    Split up and cleaned openImage()

--- script.js
+++ script.js
@@ -1,45 +1,62 @@
-// Gets the hamburger icon and menu
+// Gets the hamburger button, hamburger icon and menu
 const hamburgerMenuBtn = document.querySelector(".hamburgerMenuBtn")
 const menu = document.querySelector(".menu")
-
 const hamburgerIcon = document.querySelector(".hamburgerIcon")
 
 // Adds event listener to menu button to toggle menu open/closed
 hamburgerMenuBtn.addEventListener('click', function () {
     menu.classList.toggle('active')
     hamburgerIcon.classList.toggle('active')
-
 })
-
 
 /**
  * Function to remove the active state of container, and close the big image
- * @param {HTMLDivElement} container 
+ * @param {HTMLElement} container 
  */
 function closeContainer(container) {
     container.classList.remove('active')
 }
- 
+
+/**
+ * Creates a container with given ID.
+ * Appends it to a parent if a parent is given.
+ * Returns container
+ * @param {string} htmlTag
+ * @param {string} id
+ * @param {HTMLElement} parent
+ * @return {HTMLDivElement}
+ */
+function createContainer(htmlTag, id, parent) {
+    const container = document.createElement(htmlTag)
+    container.id = id
+
+    if(parent) {
+        parent.appendChild(container)
+    }
+
+    return container
+}
+
 /** 
  * Open up bigger image, when small image is clicked
  * @param {MouseEvent} event
  */
 function openImage(event) {
     // Gets containers
+    /** @type {HTMLDivElement} */
     let container = document.getElementById('container')
     let bigImageContainer = document.getElementById('bigImageContainer')
     let shadowContainer = document.getElementById('shadowContainer')
+    let subPageContainer = document.getElementById('subPageContainer')
 
-    // If no containers are found, create containers
+    // If no container, create container
     if (!container) {
-        container = document.createElement('div')
-        container.id = 'container'
+        container = createContainer('DIV', 'container', subPageContainer)
     }
     
+    // If no shadowContainer, create container
     if (!shadowContainer) {
-        shadowContainer = document.createElement('div')
-        shadowContainer.id = 'shadowContainer'
-        container.appendChild(shadowContainer)
+        shadowContainer = createContainer('DIV', 'shadowContainer', container)
 
         // Adds click eventlistener to shadowContainer to close state
         shadowContainer.addEventListener('click', function () {
@@ -47,14 +64,10 @@ function openImage(event) {
         })
     }
 
+    // If no bigImageContainer, create container
     if (!bigImageContainer) {
-        bigImageContainer = document.createElement('div')
-        bigImageContainer.id = 'bigImageContainer'
-        container.appendChild(bigImageContainer)
+        bigImageContainer = createContainer('DIV', 'bigImageContainer', container)
     }
-
-    // gets container
-    let subPageContainer = document.getElementById('subPageContainer')
     
      /** @type {HTMLImageElement} */
     const clickedImage = event.target
@@ -68,9 +81,6 @@ function openImage(event) {
     
     // Add image copy to bigImageContainer
     bigImageContainer.appendChild(createImage)
-
-    // Add container to subContainer
-    subPageContainer.appendChild(container)
 
     // Show container
     container.classList.add('active')
